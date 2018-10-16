0249aaed0cd4344d595f9a4348b6df671ac4ace9
ksugawara61 <katsuya@kokohadoko.net>
2018-10-17 06:06:47 +0900

    [add] Gradleの初期設定を追加

---
+++ app/src/main/java/net/k2o_info/qiitaview/MainActivity.kt
@@ -0,0 +1,12 @@
+package net.k2o_info.qiitaview
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
