e9c2e8e70c656571c2c024f2bc0e3a2f4fe52578
ksugawara61 <katsuya@kokohadoko.net>
2018-10-18 00:47:28 +0900

    [add] refs #4 ライセンスリストとご意見・ご要望へ遷移する処理を実装

--- app/src/main/java/net/k2o_info/qiitaview/view/fragment/SettingFragment.kt
+++ app/src/main/java/net/k2o_info/qiitaview/view/fragment/SettingFragment.kt
@@ -1,8 +1,14 @@
 package net.k2o_info.qiitaview.view.fragment
 
+import android.content.Intent
 import android.content.pm.PackageManager
+import android.net.Uri
 import android.os.Bundle
+import android.support.customtabs.CustomTabsIntent
+import android.support.v4.content.ContextCompat
+import android.support.v7.preference.Preference
 import android.support.v7.preference.PreferenceFragmentCompat
+import com.google.android.gms.oss.licenses.OssLicensesMenuActivity
 import net.k2o_info.qiitaview.R
 import timber.log.Timber
 
@@ -43,6 +49,34 @@ class SettingFragment : PreferenceFragmentCompat() {
             Timber.e(e.message)
         }
 
+    }
+
+    /**
+     * 設定の項目選択時のイベント
+     *
+     * @param preference プリファレンス
+     * @return true
+     */
+    override fun onPreferenceTreeClick(preference: Preference): Boolean {
+        when (preference.key) {
+            getString(R.string.pref_opinion_key) -> {
+                // ご意見・ご要望が選択された時にWebでご意見・ご要望ページへ遷移
+                val tabsIntent = CustomTabsIntent.Builder()
+                        .setShowTitle(true)
+                        .setToolbarColor(ContextCompat.getColor(context!!, R.color.colorPrimary))
+                        .build()
+                tabsIntent.launchUrl(context!!, Uri.parse(getString(R.string.opinion_form_url)))
+            }
+
+            getString(R.string.pref_license_key) -> {
+                // ライセンスが選択された時にライセンスリストを表示
+                OssLicensesMenuActivity.setActivityTitle(getString(R.string.pref_license))
+                startActivity(Intent(activity, OssLicensesMenuActivity().javaClass))
+            }
+
+        }
+
+        return super.onPreferenceTreeClick(preference)
     }
 
 }
